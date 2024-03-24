3cf75e26a059d3704136c30afd4815c61247f1ad
Infarcactus <mathieu.fournet04@orange.fr>
2024-03-24 14:09:29 +0100

    Implemented new functionality
    Implemented
    Add_Custom_Stop_Sound
    Add_Custom_Replace_Sound

--- Sound_Util.lua
+++ Sound_Util.lua
@@ -11,38 +11,34 @@ function Add_Custom_Sound_Global(modID)
     local mod = SMODS.findModByID(modID)
     for _, filename in ipairs(love.filesystem.getDirectoryItems(mod.path .. 'Assets')) do
         local extension = string.sub(filename, -4)
-        if extension == '.ogg' or extension == '.mp3' or extension=='.wav' then --please avoid using .mp3 files
+        if extension == '.ogg' or extension == '.mp3' or extension=='.wav' then --please use .ogg or .wav files
             local sound = nil
             local sound_code = string.sub(filename, 1, -5)
             --sendDebugMessage("path: " .. mod.path .. 'Assets/' .. filename)
             sound = {sound = love.audio.newSource(mod.path .. 'Assets/' .. filename, 'static')}
             sound.sound_code = sound_code
-            table.insert(Custom_Sounds,sound)
+            Custom_Sounds[sound_code]=sound
         end
     end
 end
 
 function Custom_Play_Sound(sound_code,stop_previous_instance, volume, pitch)
-    for _, s in pairs(Custom_Sounds) do
-        if s.sound_code == sound_code then
-            volume = volume or 1
-            if pitch then
-                s.sound:setPitch(pitch)
-            else
-                s.sound:setPitch(1)
-            end
-            local sound_vol = volume*(G.SETTINGS.SOUND.volume/100.0)*(G.SETTINGS.SOUND.game_sounds_volume/100.0)
-            if sound_vol <= 0 then
-                s.sound:setVolume(0)
-            else
-                s.sound:setVolume(sound_vol)
-            end
-            if stop_previous_instance and s.sound:isPlaying() then
-                s.sound:stop()
-            end
-            love.audio.play(s.sound)
-            return true
+    if Custom_Sounds[sound_code] then
+        local s = Custom_Sounds[sound_code]
+        stop_previous_instance = (stop_previous_instance == nil) and true or stop_previous_instance
+        volume = volume or 1
+        s.sound:setPitch(pitch or 1)
+        local sound_vol = volume*(G.SETTINGS.SOUND.volume/100.0)*(G.SETTINGS.SOUND.game_sounds_volume/100.0)
+        if sound_vol <= 0 then
+            s.sound:setVolume(0)
+        else
+            s.sound:setVolume(sound_vol)
         end
+        if stop_previous_instance and s.sound:isPlaying() then
+            s.sound:stop()
+        end
+        love.audio.play(s.sound)
+        return true
     end
     return false
 end
@@ -52,22 +48,58 @@ local Custom_Stop_Sound = {}
 function Add_Custom_Stop_Sound(sound_code)
     if type(sound_code)=="table" then
         for _,s_c in ipairs(sound_code) do
-            table.insert(Custom_Stop_Sound,s_c)
+            if type(s_c)=="string" then
+                Custom_Stop_Sound[s_c] = true
+            else
+                return false
+            end
+        end
+    elseif type(sound_code)=="string" then
+        table.insert(Custom_Stop_Sound,sound_code)
+    else
+        return false
+    end
+    return true
+end
+
+local Custom_Replace_Sound = {}
+
+function Add_Custom_Replace_Sound(replace_code_table)
+    if type(replace_code_table)=="table" then
+        for original_sound_code,replacement_sound_code in pairs(replace_code_table) do
+            if type(replacement_sound_code)=="table" or type(replacement_sound_code)=="string" then
+                Custom_Replace_Sound[original_sound_code] = replacement_sound_code
+            else
+                return false
+            end
         end
     else
-        table.insert(Custom_Stop_Sound,sound_code)
+        return false
     end
+    return true
 end
 
 local Original_play_sound = play_sound
 function play_sound(sound_code, per, vol)
-    for _, stopped_sound in ipairs(Custom_Stop_Sound) do
-        if stopped_sound == sound_code then
+    if Custom_Replace_Sound[sound_code] then
+        if type(Custom_Replace_Sound[sound_code]) == "table" then
+            local sound_args = Custom_Replace_Sound[sound_code]
+            sendDebugMessage("testing " .. tostring(sound_args.sound_code))
+            Custom_Play_Sound(sound_args.sound_code,sound_args.stop_previous_instance,sound_args.volume,sound_args.pitch)
+            if not(sound_args.continue_base_sound) then
+                return
+            end
+        else
+            Custom_Play_Sound(Custom_Replace_Sound[sound_code])
             return
         end
     end
-    --sendDebugMessage("sound_code : " .. sound_code)
+    if Custom_Stop_Sound[sound_code] then
+        return
+    end
+    sendDebugMessage("sound_code : " .. sound_code)
     return Original_play_sound(sound_code, per, vol)
 end
+
 ----------------------------------------------
 ------------MOD CODE END----------------------
--- example_mod/Jojo's Tarots/Jojo.lua
+++ example_mod/Jojo's Tarots/Jojo.lua
@@ -6,8 +6,15 @@
 ----------------------------------------------
 ------------MOD CODE -------------------------
 
-function SMODS.INIT.jojo()
+function SMODS.INIT.jojo_tarot()
     Add_Custom_Sound_Global("jojo_tarot")
+    -- these were added but not used in my mod to explain other functions better (they work)
+    Add_Custom_Replace_Sound({button = "The World"})
+    Add_Custom_Replace_Sound({paper1 = {sound_code ="The World",volume = 0.01} })
+    Add_Custom_Replace_Sound({cardSlide1 = {sound_code ="Death",stop_previous_instance = true, volume = 1,pitch=1,continue_base_sound=true} })
+    Add_Custom_Stop_Sound({"whoosh","something"})
+    Add_Custom_Stop_Sound("whoosh1")
+    -- end of added functions for explanation
 end
 
 local Orginial_Card_use_consumeable = Card.use_consumeable
